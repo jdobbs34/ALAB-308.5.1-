3a40798bd0c9bb6af0849cd9f5cfd2f5178fa8bb
jdobbs34 <dobbsjason34@yahoo.com>
2025-12-10 20:25:47 -0700

    3rd coomit

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-function addsum [] {
-let sum = [1, 2, 3].reduce((partialSum, a) => partialSum + a, 0);
-addsum(sum); 
-}
+// Take an array of numbers and return the sum.
+let myArray = [1, 3, 5, 7, 9];
+let totalNum = 0;
+function numSum(numbers) {
+
+  for (let i = 0; i < anArray.length; i++) {
+    totalNum += anArray[i];
+
+  }
+  console.log(totalNum);
+} 
+sumNum();
   
 
+// Take an array of numbers and return the average.
+let avgArray = [1, 3, 5, 7, 9];
+let avgSum = 0;
+
+function avgNum(median) {
+  for (let i = 0; i < avgArray.length; i++) {
+    avgNum += avgArray[i];
+  }
+  console.log{avgSum / avgArray.length}
+}
+avgNum();
+
+// Take an array of strings and return the longest string.
  
